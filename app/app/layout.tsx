e925f79a62348988361572c5807baa2a01233b5c
Meverick9 <agroflightgis@gmail.com>
2026-01-14 22:05:22 +0200

    Add Google Analytics component to RootLayout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,9 +1,12 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-LJJL1MFPLD" />
     </html>
   )
 }
