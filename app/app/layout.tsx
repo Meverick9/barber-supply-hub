f98cb3e235b8ecea765aabf430db80e6a82cb931
Meverick9 <agroflightgis@gmail.com>
2026-01-14 22:11:54 +0200

    Fix import statement for GoogleAnalytics

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from 'next/third-parties/google'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
