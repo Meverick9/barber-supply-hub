5bd70b042ef5033db389196c828172dff3c0b36c
Meverick9 <agroflightgis@gmail.com>
2026-01-14 22:19:03 +0200

    Integrate Google Tag Manager using Script tags
    Replaced GoogleAnalytics component with Script tags for Google Tag Manager integration.

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,12 +1,25 @@
-import { GoogleAnalytics } from "next/third-parties/google";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LJJL1MFPLD"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LJJL1MFPLD');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
-      <GoogleAnalytics gaId="G-LJJL1MFPLD" />
     </html>
   )
 }
